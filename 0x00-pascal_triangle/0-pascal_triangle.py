1d388fcf68d38c55eb9745473a26ac5cf9d9f480
PaulaMaina <kabura.mainap@gmail.com>
2024-07-25 19:03:17 +0000

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#usr/bin/python3
+"""Pascal's Triangle"""
+def pascal_triangle(n):
+    """Returns a list of integers representing the Pascal's triangle of n"""
+    result = [];
+
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            x = 1
+            for j in range(1, i + 1):
+                level.append(x)
+                x = x * (i - j) // j
+            result .append(level)
+
+    return result
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+This is a readme for the Pascal's triangle project
