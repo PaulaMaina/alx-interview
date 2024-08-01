f523ee25c02cd23053d3bc3703c770de2fadda46
PaulaMaina <kabura.mainap@gmail.com>
2024-08-01 18:02:11 +0000

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,11 +10,11 @@ def canUnlockAll(boxes):
 
     while len(unchecked_boxes) > 0:
         box_index = unchecked_boxes.pop()
-        
+
         if not box_index or box_index >= all_boxes or box_index < 0:
             continue
         if box_index not in checked_boxes:
             unchecked_boxes = unchecked_boxes.union(boxes[box_index])
             checked_boxes.add(box_index)
 
-        return all_boxes == len(checked_boxes)
+    return all_boxes == len(checked_boxes)
