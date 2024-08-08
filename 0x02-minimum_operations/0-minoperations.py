6c8032e86ead8f00ac725e5e2e8a9d45e76f590a
PaulaMaina <kabura.mainap@gmail.com>
2024-08-08 13:40:03 +0000

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+def minOperations(n):
+    """Calculates the fewest number of operations to result in H characters"""
+    if n < 2:
+        return 0
+    operations = 0
+    root = 2
+    whle root <= n:
+        if n% root == 0:
+            operations += root
+            n = n / root
+            root -= 1
+        root += 1
+
+    return operations
