2cac0e2cd02d696fcc37c3b3dc359e527a373f25
PaulaMaina <kabura.mainap@gmail.com>
2024-08-15 10:16:14 +0000

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Reads stdin line by line and computes the metrics"""
+import sys
+
+
+def compute_metrics(stat_dict, filesize):
+    print("File size: {}".format(filesize))
+    for key, value in sorted(stat_dict.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+    filesize = 0
+    stat_code = 0
+    counter = 0
+    stat_dict = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+
+    try:
+        for line in sys.stdin:
+            par_line = line.split()
+            par_line  = par_line[::-1]
+
+            if len(par_line) > 2:
+                counter += 1
+
+                if counter <= 10:
+                    filesize += int(par_line[0])
+                    stat_code = par_line[1]
+
+                    if counter == 10:
+                        compute_metrics(stat_dict, file_size)
+                        counter = 0
+
+    finally:
+        compute_metrics(stat_dict, filesize)
