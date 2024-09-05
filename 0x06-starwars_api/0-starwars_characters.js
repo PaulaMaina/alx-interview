36d6d574abb8b274709b2ae611b7b561f553e17a
PaulaMaina <kabura.mainap@gmail.com>
2024-09-05 12:07:21 +0000

    Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+req('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function(error, res, body) {
+  if (error) throw error;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+
+const exactOrder = (actors, p) => {
+  if (p === actors.length) return;
+  req(actors[p], function(error, res, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, p + 1);
+  });
+};
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,1 @@
+This is a readme for for the Star Wars API project
